2bc8d0c0bba8de7f2ebfeeabe5e932050e9aae83
User <User@DESKTOP-3G79H5S>
2023-10-11 14:14:45 +0700

    Добавьте файлы проекта.

---
+++ FileManager/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace FileManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Desktop = new System.Windows.Forms.Button();
+            this.button_Documents = new System.Windows.Forms.Button();
+            this.button_Pictures = new System.Windows.Forms.Button();
+            this.button_Music = new System.Windows.Forms.Button();
+            this.button_Video = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel_FilesList = new System.Windows.Forms.FlowLayoutPanel();
+            this.Back = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button_Desktop
+            // 
+            this.button_Desktop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_Desktop.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button_Desktop.Location = new System.Drawing.Point(3, 3);
+            this.button_Desktop.Name = "button_Desktop";
+            this.button_Desktop.Size = new System.Drawing.Size(111, 23);
+            this.button_Desktop.TabIndex = 0;
+            this.button_Desktop.Text = "Рабочий стол";
+            this.button_Desktop.UseVisualStyleBackColor = true;
+            this.button_Desktop.Click += new System.EventHandler(this.button_Desktop_Click);
+            // 
+            // button_Documents
+            // 
+            this.button_Documents.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_Documents.Location = new System.Drawing.Point(3, 32);
+            this.button_Documents.Name = "button_Documents";
+            this.button_Documents.Size = new System.Drawing.Size(111, 23);
+            this.button_Documents.TabIndex = 1;
+            this.button_Documents.Text = "Мои документы";
+            this.button_Documents.UseVisualStyleBackColor = true;
+            this.button_Documents.Click += new System.EventHandler(this.button_Documents_Click);
+            // 
+            // button_Pictures
+            // 
+            this.button_Pictures.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_Pictures.Location = new System.Drawing.Point(3, 61);
+            this.button_Pictures.Name = "button_Pictures";
+            this.button_Pictures.Size = new System.Drawing.Size(111, 23);
+            this.button_Pictures.TabIndex = 2;
+            this.button_Pictures.Text = "Картинки";
+            this.button_Pictures.UseVisualStyleBackColor = true;
+            this.button_Pictures.Click += new System.EventHandler(this.button_Pictures_Click);
+            // 
+            // button_Music
+            // 
+            this.button_Music.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_Music.Location = new System.Drawing.Point(3, 90);
+            this.button_Music.Name = "button_Music";
+            this.button_Music.Size = new System.Drawing.Size(111, 23);
+            this.button_Music.TabIndex = 3;
+            this.button_Music.Text = "Музыка";
+            this.button_Music.UseVisualStyleBackColor = true;
+            this.button_Music.Click += new System.EventHandler(this.button_Music_Click);
+            // 
+            // button_Video
+            // 
+            this.button_Video.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_Video.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button_Video.Location = new System.Drawing.Point(3, 119);
+            this.button_Video.Name = "button_Video";
+            this.button_Video.Size = new System.Drawing.Size(111, 23);
+            this.button_Video.TabIndex = 4;
+            this.button_Video.Text = "Видео";
+            this.button_Video.UseVisualStyleBackColor = true;
+            this.button_Video.Click += new System.EventHandler(this.button_Video_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button_Desktop);
+            this.panel1.Controls.Add(this.button_Video);
+            this.panel1.Controls.Add(this.button_Documents);
+            this.panel1.Controls.Add(this.button_Music);
+            this.panel1.Controls.Add(this.button_Pictures);
+            this.panel1.Location = new System.Drawing.Point(12, 53);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(121, 152);
+            this.panel1.TabIndex = 5;
+            // 
+            // panel_FilesList
+            // 
+            this.panel_FilesList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel_FilesList.Location = new System.Drawing.Point(149, 53);
+            this.panel_FilesList.Name = "panel_FilesList";
+            this.panel_FilesList.Size = new System.Drawing.Size(633, 466);
+            this.panel_FilesList.TabIndex = 6;
+            // 
+            // Back
+            // 
+            this.Back.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Back.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Back.Font = new System.Drawing.Font("Lucida Sans Unicode", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Back.Location = new System.Drawing.Point(66, 492);
+            this.Back.Name = "Back";
+            this.Back.Size = new System.Drawing.Size(77, 27);
+            this.Back.TabIndex = 7;
+            this.Back.Text = "< Назад";
+            this.Back.UseVisualStyleBackColor = false;
+            this.Back.Click += new System.EventHandler(this.Back_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(149, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(633, 20);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(843, 531);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Back);
+            this.Controls.Add(this.panel_FilesList);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Desktop;
+        private System.Windows.Forms.Button button_Documents;
+        private System.Windows.Forms.Button button_Pictures;
+        private System.Windows.Forms.Button button_Music;
+        private System.Windows.Forms.Button button_Video;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel panel_FilesList;
+        private System.Windows.Forms.Button Back;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ FileManager/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileManager
+{
+    public partial class Form1 : Form
+    {
+        private string currentLocation;
+        private string stringcurrentLocation;
+
+        public Form1()
+        {
+            InitializeComponent();
+            DisplayFiles(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Desktop_Click(object sender, EventArgs e)
+        {
+
+            DisplayFiles(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+        }
+
+        private void button_Documents_Click(object sender, EventArgs e)
+        {
+            DisplayFiles(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+        }
+
+        private void button_Pictures_Click(object sender, EventArgs e)
+        {
+            DisplayFiles(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures));
+        }
+
+        private void button_Music_Click(object sender, EventArgs e)
+        {
+            DisplayFiles(Environment.GetFolderPath(Environment.SpecialFolder.MyMusic));
+        }
+
+        private void button_Video_Click(object sender, EventArgs e)
+        {
+            DisplayFiles(Environment.GetFolderPath(Environment.SpecialFolder.MyVideos)); 
+        }
+        private void DisplayFiles(string filePath)
+        {
+            currentLocation = filePath;
+            stringcurrentLocation = "";
+            panel_FilesList.Controls.Clear();
+            string[] filesList = Directory.GetDirectories(filePath).Concat(Directory.GetFiles(filePath)).ToArray();
+            for (int i = 0; i < filesList.Length - 1; i++)
+            {
+                bool isHidden = ((File.GetAttributes(filesList[i]) & FileAttributes.Hidden) == FileAttributes.Hidden);
+
+                if (!isHidden)
+                {
+                    var startOfName = filesList[i].LastIndexOf("\\");
+                    var fileName = filesList[i].Substring(startOfName + 1, filesList[i].Length - (startOfName + 1));
+
+                    Button newButton = new Button();
+                    newButton.Text = fileName;
+                    newButton.Name = filesList[i];
+                    newButton.Location = new Point(70, 70);
+                    newButton.Size = new Size(200, 60);
+                    newButton.TextAlign = ContentAlignment.MiddleLeft;
+                    newButton.Padding = new Padding(24, 0, 0, 0);
+                    newButton.BackColor = Color.Pink;
+                    newButton.FlatStyle = FlatStyle.Flat;
+                    panel_FilesList.Controls.Add(newButton);
+                    newButton.Click += button_Click_Open;
+                }
+            }
+        }
+        
+        private void button_Click_Open(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string filePath = button.Name;
+
+            try
+            {
+                DisplayFiles(filePath);
+            }
+            catch (Exception ex)
+            {
+                var process = new System.Diagnostics.Process();
+                process.StartInfo = new System.Diagnostics.ProcessStartInfo() { UseShellExecute = true, FileName = filePath };
+                process.Start();
+            }
+        }
+
+        private void Back_Click(object sender, EventArgs e)
+        {
+            var previousFolder = this.currentLocation.Substring(0, this.currentLocation.LastIndexOf("\\"));
+            DisplayFiles(previousFolder);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
